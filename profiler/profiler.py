1c570a09a81fd1aa7c50aebd843e05284fdd301f
McWilton Chikwenengere <mcwilton85@gmail.com>
2024-09-03 03:34:28 -0500

    Update profiler.py

--- profiler/profiler.py
+++ profiler/profiler.py
@@ -1,5 +1,5 @@
 from pyspark.sql import DataFrame, SparkSession
-from pyspark.sql.functions import when, col, count
+from pyspark.sql.functions import col, isnan, count, when, lit, sum as spark_sum, mean as spark_mean, variance as spark_variance, stddev, skewness, kurtosis
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -45,17 +45,48 @@ class Profiler:
         self.df = self.spark.read.orc(file_path)
         return self.df
 
+    # def check_outliers(self):
+    #     outliers = {}
+    #     for col in self.df.columns:
+    #         quantiles = self.df.approxQuantile(col, [0.25, 0.75], 0.05)
+    #         if quantiles and len(quantiles) == 2:
+    #             iqr = quantiles[1] - quantiles[0]
+    #             lower_bound = quantiles[0] - 1.5 * iqr
+    #             upper_bound = quantiles[1] + 1.5 * iqr
+    #             outliers[col] = self.df.filter((self.df[col] < lower_bound) | (self.df[col] > upper_bound)).count()
+    #     return outliers
+
     def check_outliers(self):
-        outliers = {}
-        for col in self.df.columns:
-            quantiles = self.df.approxQuantile(col, [0.25, 0.75], 0.05)
-            if quantiles and len(quantiles) == 2:
-                iqr = quantiles[1] - quantiles[0]
-                lower_bound = quantiles[0] - 1.5 * iqr
-                upper_bound = quantiles[1] + 1.5 * iqr
-                outliers[col] = self.df.filter((self.df[col] < lower_bound) | (self.df[col] > upper_bound)).count()
-        return outliers
-    
+        if self.df is None:
+            raise ValueError("No DataFrame loaded. Please load a dataset first.")
+        
+        outlier_summary = {}
+        
+        for column in self.df.columns:
+            dtype = [field.dataType for field in self.df.schema.fields if field.name == column][0]
+
+            # Check if column is numeric
+            if isinstance(dtype, NumericType):
+                quantiles = self.df.approxQuantile(column, [0.25, 0.75], 0.05)
+                q1 = quantiles[0]
+                q3 = quantiles[1]
+                iqr = q3 - q1
+                lower_bound = q1 - 1.5 * iqr
+                upper_bound = q3 + 1.5 * iqr
+
+                # Count outliers
+                num_outliers = self.df.filter((col(column) < lower_bound) | (col(column) > upper_bound)).count()
+
+                outlier_summary[column] = {
+                    "lower_bound": lower_bound,
+                    "upper_bound": upper_bound,
+                    "num_outliers": num_outliers
+                }
+            else:
+                outlier_summary[column] = "Column is not numeric; skipping outlier detection."
+
+        return outlier_summary
+        
     def check_null_empty_values(self):
         nulls = self.df.select([F.count(F.when(F.col(c).isNull(), c)).alias(c) for c in self.df.columns])
         empties = self.df.select([F.count(F.when(F.col(c) == "", c)).alias(c) for c in self.df.columns])
@@ -86,6 +117,66 @@ class Profiler:
         
         return self.df.describe().show()
     
+    def profile_statistics(self):
+        if self.df is None:
+            raise ValueError("No DataFrame loaded. Please load a dataset first.")
+        
+        summary = {}
+        
+        for column in self.df.columns:
+            col_data = self.df.select(column)
+
+            # Sample size
+            sample_size = self.df.count()
+
+            # Null count
+            null_count = col_data.filter(col(column).isNull()).count()
+
+            # Null types and indices
+            null_types = col_data.filter(col(column).isNull()).distinct().collect()
+            null_types_index = {
+                null_type[column]: col_data.filter(col(column).isNull()).rdd.zipWithIndex().filter(lambda x: x[0][column] == null_type[column]).map(lambda x: x[1]).collect()
+                for null_type in null_types
+            }
+
+            # Data type representation
+            data_type_representation = col_data.groupBy(col(column).cast("string")).count().withColumn("percentage", col("count")/sample_size * 100).select("percentage").collect()
+
+            # Descriptive statistics
+            numeric_col = col_data.select(column).where(col(column).cast("float").isNotNull())
+            median_absolute_deviation = None # Need custom calculation
+            sum_ = numeric_col.agg(spark_sum(col(column))).first()[0]
+            mean_ = numeric_col.agg(spark_mean(col(column))).first()[0]
+            variance_ = numeric_col.agg(spark_variance(col(column))).first()[0]
+            stddev_ = numeric_col.agg(stddev(col(column))).first()[0]
+            skewness_ = numeric_col.agg(skewness(col(column))).first()[0]
+            kurtosis_ = numeric_col.agg(kurtosis(col(column))).first()[0]
+
+            # Number of zeros
+            num_zeros = numeric_col.filter(col(column) == 0).count()
+
+            # Number of negatives
+            num_negatives = numeric_col.filter(col(column) < 0).count()
+
+            # Aggregate results
+            summary[column] = {
+                "sample_size": sample_size,
+                "null_count": null_count,
+                "null_types": null_types,
+                "null_types_index": null_types_index,
+                "data_type_representation": data_type_representation,
+                "median_absolute_deviation": median_absolute_deviation,
+                "sum": sum_,
+                "mean": mean_,
+                "variance": variance_,
+                "stddev": stddev_,
+                "skewness": skewness_,
+                "kurtosis": kurtosis_,
+                "num_zeros": num_zeros,
+                "num_negatives": num_negatives,
+            }
+        
+        return summary
     def missing_values(self):
         if self.df:
             return self.df.select([count(when(col(c).isNull(), c)).alias(c) for c in self.df.columns])
@@ -164,3 +255,6 @@ class Profiler:
             text = message.as_string()
             server.sendmail(from_email, recipient, text)
             server.quit()
+
+
+# pypi-AgEIcHlwaS5vcmcCJDA3ZTk3ZWQzLTE1YjItNGQ0Ny05ZTg3LWI5ZTczYzFlZGFkNwACKlszLCJmODgwODM4Yy0zNTNhLTRkYzEtOWE0NS1kMjkyN2U2NDU3MTYiXQAABiBDb8Y0f6azgdmDlIJsR5oegiN_JyiCGpw_z3ffVrJhHw
